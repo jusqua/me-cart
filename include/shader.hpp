6939a2932a5abd49198f1d4412c5247b9b68105a
Ádrian Gama <adrian.gama.se@gmail.com>
2024-03-18 23:44:37 -0300

    Add shader class can now import geometry shaders

--- include/shader.hpp
+++ include/shader.hpp
@@ -8,10 +8,14 @@ class Shader {
  public:
   unsigned int ID;
   Shader(const char *vertexPath, const char *fragmentPath);
+  Shader(const char *vertexPath, const char *geometryPath, const char *fragmentPath);
   ~Shader();
 
   void activate(void);
   void destroy(void);
+
+ private:
+  void linkShaders(std::vector<unsigned int> shaders);
 };
 
 #endif  // !MECART_SHADER_HPP
--- src/shader.cpp
+++ src/shader.cpp
@@ -1,30 +1,24 @@
 #include <shader.hpp>
 
-// Shader constructor
+// Shader constructor with vertex and fragment shaders
 Shader::Shader(const char *vertexPath, const char *fragmentPath) {
-  int success;
-  char log[512];
   // Import shaders
   auto vertexShader = importShader(vertexPath, GL_VERTEX_SHADER);
   auto fragmentShader = importShader(fragmentPath, GL_FRAGMENT_SHADER);
 
   // Attach shaders to program
-  ID = glCreateProgram();
-  glAttachShader(ID, vertexShader);
-  glAttachShader(ID, fragmentShader);
-  glLinkProgram(ID);
+  linkShaders({vertexShader, fragmentShader});
+}
 
-  // Check for link errors
-  glGetProgramiv(ID, GL_LINK_STATUS, &success);
-  if (!success) {
-    glGetProgramInfoLog(ID, 512, NULL, log);
-    std::cerr << "ERROR: Failed to link shaders\n"
-              << log << std::endl;
-  }
+// Shader constructor with vertex, geometry and fragment shaders
+Shader::Shader(const char *vertexPath, const char *geometryPath, const char *fragmentPath) {
+  // Import shaders
+  auto vertexShader = importShader(vertexPath, GL_VERTEX_SHADER);
+  auto geometryShader = importShader(geometryPath, GL_GEOMETRY_SHADER);
+  auto fragmentShader = importShader(fragmentPath, GL_FRAGMENT_SHADER);
 
-  // Delete shaders, they are not useful after attaching
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+  // Attach shaders to program
+  linkShaders({vertexShader, geometryShader, fragmentShader});
 }
 
 // Shader destructor
@@ -40,4 +34,27 @@ void Shader::activate(void) {
 // Destroy shader program
 void Shader::destroy(void) {
   glDeleteProgram(ID);
+}
+
+void Shader::linkShaders(std::vector<unsigned int> shaders) {
+  int success;
+  char log[512];
+
+  // Create shader program
+  ID = glCreateProgram();
+  for (auto shader : shaders)
+    glAttachShader(ID, shader);
+  glLinkProgram(ID);
+
+  // Check for link errors
+  glGetProgramiv(ID, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(ID, 512, NULL, log);
+    std::cerr << "ERROR: Failed to link shaders\n"
+              << log << std::endl;
+  }
+
+  // Destroy unused shaders
+  for (auto shader : shaders)
+    glDeleteShader(shader);
 }
